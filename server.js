29314359d1e5ad2151902cced36e58d3aa698279
masattuber <uvervivid1231@icloud.com>
2025-07-22 13:40:14 +0900

    renderスリーブとモンゴDB削除対策API

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //http://localhost:3000/
 const express = require("express");
-const app = express( );
+const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -22,6 +22,7 @@ mongoose
   process.exit(1); // エラー時にプロセス終了
     } );
 
+// メモリ状況管理ログ
 setInterval(() => {
   const used = process.memoryUsage();
   console.log(`Heap Used: ${Math.round(used.heapUsed / 1024 / 1024)} MB`);
@@ -37,6 +38,39 @@ app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 //画像アップロード用のAPIエンドポイント
 app.use("/api/upload", uploadRoute);
+
+//　renderのスリーブ防止策、
+app.get("/ping", (req, res) => {
+  res.status(200).send("renderのスリーブ防止用");
+});
+
+//モンゴDB削除対策API, ユーザー数も返す
+const User = require("./models/User");  // Userスキーマ読込
+const { timeStamp, error } = require("console");
+
+app.get("/ping/db", async (req, res) => {
+
+  try {
+   /* countDocuments カウントを返すためにメタデータを使用することはありません。代わりに、不正なシャットダウンが行われたり、
+    シャーディングされたクラスターに孤立したドキュメントが存在したりしても、
+    ドキュメントを集計して正確なカウントを返します。
+    */
+    const userCount = await User.countDocuments();
+    res.status(200).json({
+      message: "MongoDBにアクセス成功",
+      userCount: userCount,
+      timestamp: new Date().toISOString()
+    });
+    // JSONでメッセージ、ユーザー数、時間を返す
+  } catch (err) {
+      console.error("ping/db エラー:", err);
+      res.status(500).json({
+        message: "MongoDBへのアクセス失敗",
+        error: err.message
+      });
+  }
+
+});
 
 app.listen( PORT, () => console.log("サーバーが起動しました。"));
 //サーバーの処理
